236a76acbb91c855697d4e14bf7fef98ad55251d
christopher <chrisolivaresdev@gmail.com>
2023-09-01 19:56:43 -0400

    changes

--- electron/main.js
+++ electron/main.js
@@ -8,7 +8,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
-    zoomFactor: 1.2,
+    zoomFactor: 1.4,
     webPreferences: {
       nodeIntegration: true,
     },
